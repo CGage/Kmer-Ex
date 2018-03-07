f747fb6bde1bce29ad9c00bd4cf4ea648cf8c214
CGage <christopher.gage@connect.qut.edu.au>
2018-03-07 13:41:10 +1000

    Update khmer.sh

--- khmer.sh
+++ khmer.sh
@@ -1,3 +1,3 @@
-#gcc preprocess.c -o preprocess
-#.preprocess /home/chris/Challenge/Data/hg38/# Path to chromosome files
+gcc preprocess.c -o preprocess
+.preprocess /home/chris/Challenge/Data/hg38/# Path to chromosome files
 python khmerExtraction.py 3 /home/chris/Challenge/Data/hg38/ ~/Challenge/Data/allGenes.txt
